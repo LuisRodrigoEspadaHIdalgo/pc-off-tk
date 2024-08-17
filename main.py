755ddbe7e8a5f3b3cab79745b13076ccd745aed1
lucho <luisrodrigoespadahidalgo@gmail.com>
2024-08-17 09:59:44 -0300

    Creacion de ventana raiz y label. Primeras configuraciones sobre la ventana

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 #Mini app con tkinter para ejecutar shutdown
 #y apagar la pc
+import tkinter as tk
+
+root = tk.Tk()
+
+message = tk.Label(root, text="Ingrese en cuantos minutos se apagará la pc:")
+message.pack()
+
+root.title("Apagar Pc")
+
+window_width = 300
+window_height = 200
+
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+root.resizable(False, False)
+root.attributes('-topmost', 1)
+root.iconbitmap('./img/kipi.ico')
+
+
+root.mainloop()
---
+++ readme.txt
@@ -0,0 +1,2 @@
+https://mothergeo-py.readthedocs.io/en/latest/development/how-to/venv-win.html
+https://www.pythontutorial.net/tkinter/
